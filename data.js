c949b842f5011dfb77af56aecf3d0e35f1959f3f
tint0520 <tint.collect@gmail.com>
2025-04-09 12:27:49 +0800

    Create data.js

---
+++ data.js
@@ -0,0 +1,94 @@
+// data.js - Tint Maps 假資料，共 6 筆
+
+const stores = [
+  {
+    id: 1,
+    name: "好時質甲",
+    lat: 25.048412,
+    lng: 121.532903,
+    address: "台北市中山區南京東路一段100號",
+    services: ["美甲"],
+    method: "預約制",
+    images: [
+      "https://example.com/store1a.jpg",
+      "https://example.com/store1b.jpg"
+    ],
+    distance: "0.5 公里",
+    description: "精緻光療設計與溫柔手足護理，打造舒適美甲體驗"
+  },
+  {
+    id: 2,
+    name: "EZEEY NAIL",
+    lat: 25.042233,
+    lng: 121.537234,
+    address: "台北市大安區忠孝東路四段200號",
+    services: ["美甲", "美睫"],
+    method: "現場/預約皆可",
+    images: [
+      "https://example.com/store2a.jpg",
+      "https://example.com/store2b.jpg"
+    ],
+    distance: "1.2 公里",
+    description: "韓系風格流行設計，專屬你的時尚指尖與迷人眼神"
+  },
+  {
+    id: 3,
+    name: "臉部光采",
+    lat: 25.037921,
+    lng: 121.543876,
+    address: "台北市信義區松壽路12號",
+    services: ["臉部保養"],
+    method: "預約制",
+    images: [
+      "https://example.com/store3a.jpg",
+      "https://example.com/store3b.jpg"
+    ],
+    distance: "2.0 公里",
+    description: "臉部護理專家，幫助肌膚回復年輕水潤光采"
+  },
+  {
+    id: 4,
+    name: "足感心動",
+    lat: 25.045789,
+    lng: 121.550432,
+    address: "台北市松山區八德路三段50號",
+    services: ["足部保養"],
+    method: "現場接客",
+    images: [
+      "https://example.com/store4a.jpg",
+      "https://example.com/store4b.jpg"
+    ],
+    distance: "2.5 公里",
+    description: "頂級足部護理服務，讓你在城市中放鬆身心"
+  },
+  {
+    id: 5,
+    name: "美睫魅力",
+    lat: 25.050321,
+    lng: 121.560123,
+    address: "台北市內湖區瑞光路168號",
+    services: ["美睫"],
+    method: "預約制",
+    images: [
+      "https://example.com/store5a.jpg",
+      "https://example.com/store5b.jpg"
+    ],
+    distance: "3.0 公里",
+    description: "專業嫁接睫毛，展現迷人自然眼神"
+  },
+  {
+    id: 6,
+    name: "SPA 養顏館",
+    lat: 25.038765,
+    lng: 121.529876,
+    address: "台北市萬華區西門町120號",
+    services: ["臉部保養", "身體SPA"],
+    method: "現場/預約皆可",
+    images: [
+      "https://example.com/store6a.jpg",
+      "https://example.com/store6b.jpg"
+    ],
+    distance: "4.0 公里",
+    description: "花卉植萃臉部與身體療癒保養，享受靜謐時光"
+  }
+];
