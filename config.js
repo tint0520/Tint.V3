dde6bf9708320bada17811a6e46b19479f76ad21
tint0520 <tint.collect@gmail.com>
2025-04-09 12:28:09 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+// config.js - Tint Maps 設定參數
+
+const CONFIG = {
+  GOOGLE_MAPS_API_KEY: "", // 請填入你的正式 API 金鑰
+  USE_CUSTOM_MAP_STYLE: true,
+  DEFAULT_CENTER: { lat: 25.046891, lng: 121.516602 },
+  DEFAULT_ZOOM: 14,
+  LOCAL_STORAGE_FAVORITES: "tint_maps_favorites"
+};
